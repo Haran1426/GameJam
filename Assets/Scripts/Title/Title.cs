241a457ba3cce4862fc6dfc38c9809b64e093d8f
Haran1426 <haranharan1105@gmail.com>
2025-07-13 10:03:24 +0900

    nooooooooooooooo

--- Assets/Scripts/Title/Title.cs
+++ Assets/Scripts/Title/Title.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
-    [SerializeField] GameObject setting; //설정창
-    // Start is called before the first frame update
+    [SerializeField] GameObject setting; // 설정창
+
     void Start()
     {
         setting.SetActive(false);
     }
+
     public void gameStart()
     {
         SceneManager.LoadScene("Tutorials");
     }
+
     public void Setting()
     {
         setting.SetActive(true);
     }
+
     public void Quit()
     {
 #if UNITY_EDITOR
@@ -28,6 +30,7 @@ public class Title : MonoBehaviour
         Application.Quit(); // 어플리케이션 종료
 #endif
     }
+
     public void X()
     {
         setting.SetActive(false);
